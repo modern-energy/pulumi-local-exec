f5190714bee49490d765078838c523fc48374824
Luke VanderHart <luke@vanderhart.net>
2021-10-25 13:32:56 -0400

    update tests to check environment

--- examples/simple-python/__main__.py
+++ examples/simple-python/__main__.py
@@ -3,8 +3,13 @@
 import pulumi
 import pulumi_local_exec
 
+command_str = pulumi.Output.from_input("env")
+msg_str = pulumi.Output.from_input("Hello world!")
+
+msg_str_2 = msg_str.apply(lambda x: x.upper())
+
 cmd = pulumi_local_exec.Command('test',
-                                command='echo $MSG',
-                                env={'MSG': 'Hello, world!'})
+                                command=command_str,
+                                env={'THE_MSG2': msg_str_2})
 
 pulumi.export('output', cmd.stdout)
--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -1,8 +1,16 @@
+import * as pulumi from "@pulumi/pulumi"
 import * as local_exec from "@modern-energy/pulumi-local-exec";
 
+
+const command_str = pulumi.output("env")
+const val = pulumi.output("some-value")
+
+console.log("value:", val)
+
 const cmd = new local_exec.Command("test", {
-    command: "echo $MSG",
-    env: {"MSG": "Hello, world!"}
+    command: command_str,
+    env: {"MSG": "Hello, world!",
+          "OUTPUT": val}
 });
 
 exports.stdout = cmd.stdout
